727c2c6690eb2aca67d28ef532f28c4db6c8b5a6
denisequarles1 <denise.quarles1@gmail.com>
2021-01-31 16:20:46 -0500

    updated files

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const path = require("path");
-
+const router = require("./routes/api.js");
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,31 +14,22 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+let db = mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workout",
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  }
+);
 
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/workout',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  );
-
-require("./routes/api-routes.js")(app);
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/index.html"));
-});
-
-app.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/exercise.html"));
-});
-
-app.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/stats.html"));
-});
+// routes
+app.use(router);
+require("./routes/html.js")(app);
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}!`);
 });
+
+module.exports = db;
