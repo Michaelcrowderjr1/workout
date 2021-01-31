95f755593722ce89a42ecfe6cf16327d55e2084f
denisequarles1 <denise.quarles1@gmail.com>
2021-01-31 18:24:54 -0500

    updated files

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 
 
-const PORT = process.env.PORT || 8060;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
 
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/workout',
