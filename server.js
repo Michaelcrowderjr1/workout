17a5b1ae90325ea2103d9ffe0c938e2e6084b52e
denisequarles1 <denise.quarles1@gmail.com>
2021-01-31 01:20:46 -0500

    updated seed and server

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 const db = require("./models");
 
 require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
