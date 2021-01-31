0b502c3302ebf3b800b2b1d54be37e4ca6a43bf6
denisequarles1 <denise.quarles1@gmail.com>
2021-01-31 16:54:36 -0500

    updated files

--- models/workout.js
+++ models/workout.js
@@ -1,52 +1,48 @@
 const mongoose = require("mongoose");
+
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-    day: {
-        type: Date,
-        default: Date.now
-    },
-
-    exercises: [
-        {
-
-            type: {
-                type: String,
-                required: "Type of exercise is required"
-            },
-
-            name: {
-                type: String,
-                required: "Name of exercise is required"
-            },
-
-            duration: {
-                type: Number,
-                required: "Duration is required"
-            },
-
-            weight: {
-                type: Number,
-            },
-
-            reps: {
-                type: Number,
-            },
-
-
-            sets: {
-                type: Number,
-            },
-
-            distance: {
-                type: Number,
-            }
-        }
-
-    ]
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+      },
+      name: {
+        type: String,
+        trim: true,
+      },
+      duration: Number,
+      weight: {
+        type: Number,
+        default: 0
+      },
+      reps: {
+        type: Number,
+        default: 0
+      },
+      sets: {
+        type: Number,
+        default: 0
+      },
+      distance: {
+        type: Number,
+        default: 0
+      }
+    }
+  ],
+  totalDuration: {
+    type: Number,
+    default: 0,
+  }
 
 });
 
-const Workout = mongoose.model("Workout", workoutSchema);
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,10 +15,31 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false });
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-app.use(require("./routes/api.js"));
-app.use(require("./routes/views.js"));
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
+require("./routes/api-routes.js")(app);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/stats.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
